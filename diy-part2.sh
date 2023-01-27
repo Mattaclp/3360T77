e10764f2698e34df2e71c03c81a055d696907974
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2023-01-27 19:02:14 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,6 +10,10 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+git clone https://github.com/sbwml/luci-app-alist.git package/luci-app-alist
+rm -rf feeds/packages/lang/golang
+svn export https://github.com/sbwml/packages_lang_golang/trunk feeds/packages/lang/golang
+
 # rm packages
 #rm -rf feeds/luci/applications/luci-app-ssr-plus/
 #rm -rf feeds/packages/net/naiveproxy/
